3af3b07f2fa63d417b1a76a09496f40e1273cef0
Do Ngoc Luu <dnluu@tctav.com>
2020-09-18 17:27:56 +0700

    add source

---
+++ src/main/java/spring/demo/api/CurdResfullApiDemoApplication.java
@@ -0,0 +1,13 @@
+package spring.demo.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurdResfullApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurdResfullApiDemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/spring/demo/api/repository/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package spring.demo.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import spring.demo.api.model.Employee;
+
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
